3f560567fd82bfebe7d33eb1f34dbf29380c2cd3
aswinaryal <shreha_0335@deerwalk.edu.np>
2015-01-26 13:47:40 +0545

    Animal pattern was added.

---
+++ DisplayAnimalPattern.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Shreha on 1/17/2015.
+ */
+public class DisplayAnimalPattern{
+    public static void main(String[] args){
+        System.out.println("\t\t\t  '___'");
+        System.out.println("\t\t\t  (\u00A9\u00A9)");
+        System.out.println("  \t  /========\\/");
+        System.out.println(" \t /|| %% ||");
+        System.out.println("\t* ||----||");
+        System.out.println("\t  \u00A5\u00A5\t\u00A5\u00A5");
+        System.out.println("\t  \"\"\t\"\"");
+    }
+}
+
+
+
+
+
